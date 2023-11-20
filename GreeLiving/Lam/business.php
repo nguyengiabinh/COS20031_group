758d2e4734cb02e7866e8ff0cebdfab601aff928
LamNguyen7 <150118604+LamNguyen7@users.noreply.github.com>
2023-11-20 23:51:01 +0700

    Update business.php

--- GreeLiving/Lam/business.php
+++ GreeLiving/Lam/business.php
@@ -24,7 +24,7 @@ if (isset($_GET['logout'])) {
         <h2>Profile</h2>
     </div>
     <div class="content">
-        <h3>This is your profile</h3>
+        <h3>This is your profile:</h3>
         <?php
         if (!isset($_GET["error"])) {
             echo "<p>Business id :" . $_SESSION["business_id"] . "</p>";
@@ -33,13 +33,14 @@ if (isset($_GET['logout'])) {
             echo "<p>Business phone number :" . $_SESSION["business_phone_number"] . "</p>";
             echo "<p>Business description :" . $_SESSION["business_description"] . "</p>" . "<br>";
 
+            echo "<h3><p>This is your job offers:<p></h3>";
             echo "<p>Job id :" . $_SESSION['job_id'] . "<p>";
             echo "<p>Business id :" . $_SESSION['business_id'] . "<p>";
             echo "<p>Job offer status :" . $_SESSION['job_offer_status'] . "<p>";
             echo "<p>Job title :" . $_SESSION['job_title'] . "<p>";
             echo "<p>Busiess name :" . $_SESSION['business_name'] . "<p>";
-            echo "<p>Job contact email :" . $_SESSION['job_contact_email'] . "<p>";
-            echo "<p>Job contact phone :" . $_SESSION['job_contact_phone'] . "<p>";
+            echo "<p>Email :" . $_SESSION['job_contact_email'] . "<p>";
+            echo "<p>Phone num :" . $_SESSION['job_contact_phone'] . "<p>";
             echo "<p>Job location :" . $_SESSION['job_location'] . "<p>";
             echo "<p>Job type :" . $_SESSION['job_type'] . "<p>";
             echo "<p>Job description :" . $_SESSION['job_description'] . "<p>";
