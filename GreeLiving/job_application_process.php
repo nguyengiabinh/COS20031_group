f18faac956adfe50add246fa148688b7108df68c
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2023-11-21 22:46:27 +0700

    redirect user back to job offer list aftre finish applying

--- GreeLiving/job_application_process.php
+++ GreeLiving/job_application_process.php
@@ -207,6 +207,7 @@ else
  
 if ($conn->query($sql) === TRUE) {
     echo "record inserted successfully";
+    header ("Location: greeliving_job_offer_list.php");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
