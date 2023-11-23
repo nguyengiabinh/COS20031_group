fc1fabf00065eb381f568bc904b981b083b1d1de
103845893 <121840549+103845893@users.noreply.github.com>
2023-11-23 21:48:36 +0700

    interview.php
    updated

--- GreeLiving/interview.php
+++ GreeLiving/interview.php
@@ -1,6 +1,10 @@
 <?php
 include 'settings.php';
 $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+session_start();
+if (!isset($_SESSION['business_id'])) {
+    header('Location: login.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +24,8 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
             </tr>
         <?php
         $uid = $_GET['uid'];
+        $jid = $_GET['jid'];
+        $sql = "INSERT INTO interview (interview_id, user_id, job_id, interview_date, interviewer_name, interview_status) VALUE (null, '$uid', '$jid', null," . $_SESSION["business_id"] . ", null)";
         $sql = "SELECT * FROM interview WHERE user_id = '$uid'";
         $result = $conn->query($sql);
         $result_check = mysqli_num_rows($result);
@@ -29,14 +35,13 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
                 echo "<tr><td>" . $row['interview_id'] . 
                 "</td><td>" . $row['user_id'] . "</td><td>" . $row['job_id'] . "</td><td>" . $row['interview_date'] .
                 "</td><td>" . $row['interviewer_name'] . "</td><td>" . $row['interview_status'] .
-                "</td><td>" . "<a href='interview.php?uid={$row['user_id']}'>Accept</a>"
-                . "</td></tr>";
+                "</td></tr>";
 
             }
             echo "</table>";
         }
         else {
-            echo "extraction job failed, no job was retrieved";
+            echo "extraction application failed, no applicant was retrieved";
         }
         $conn -> close();
         ?>
