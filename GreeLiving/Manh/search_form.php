86a8f99c96a0945c95b4500d69c5c42a4eef4d34
K1r4cod3 <manhph.2508@gmail.com>
2023-11-26 16:03:38 +0700

    Update search_form.php

--- GreeLiving/Manh/search_form.php
+++ GreeLiving/Manh/search_form.php
@@ -4,7 +4,7 @@
     <title>Student Search</title>
 </head>
 <body>
-    <h2>Student Search</h2>
+    <h2>Search</h2>
     <form action="./process_search_form.php" method="get">
         <label for="search">Search by Job Offer's name:</label>
         <input type="text" name="search" id="search" required>
